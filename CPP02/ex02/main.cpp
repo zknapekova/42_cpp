53bb78ba2c5a7cc0b213ab4890c22c2c1d10edc4
Zuzana Knapekova <z.knapekova@gmail.com>
2026-03-16 17:11:09 +0100

    add more tests

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -12,6 +12,26 @@
 
 #include "Fixed.hpp"
 
+
+static void	test(Fixed const &res, float expected, std::string name)
+{
+	std::cout << "TEST: " << name << ": ";
+	if (res.toFloat() == expected)
+		std::cout << " PASSED\n";
+	else
+		std::cout << "FAILED: expected: " << expected << ", got: " << res << std::endl;
+}
+
+static void	test_comp(bool res, bool expected, std::string name)
+{
+	std::cout << "TEST: " << name << ": ";
+	if (res == expected)
+		std::cout << " PASSED\n";
+	else
+		std::cout << "FAILED: expected: " << expected << ", got: " << res << std::endl;
+}
+
+
 int main( void ) {
 
 	int	i = 10;
@@ -21,190 +41,86 @@ int main( void ) {
 	Fixed r;
 
 	//addition:
-	r = fix_i + Fixed(20);
-	std::cout << "## expected: 50, got: " << r << std::endl;
-
-	r = fix_f + Fixed(20);
-	std::cout << "## expected: 40.5, got: " << r << std::endl;
-
-	r = fix_f + f;
-	std::cout << "## expected: 23, got: " << r << std::endl;
-
-	r = fix_i + i;
-	std::cout << "## expected: 40, got: " << r << std::endl;
-
-	r = i + fix_i;
-	std::cout << "## expected: 40, got: " << r << std::endl;
-
-	r = f + fix_i;
-	std::cout << "## expected: 32.5, got: " << r << std::endl;
+	test(fix_i + Fixed(20), 50.0f, "Addition of two fixed objects");
+	test(fix_f + Fixed(20), 40.5f, "Addition of two fixed objects");
+	test(fix_f + f, 23.0f, "Addition of a fixed object and float");
+	test(fix_i + i, 40.0f, "Addition of a fixed object and int");
+	test(i + fix_i, 40.0f, "Addition of an int and fixed object");
+	test(f + fix_i, 32.5f, "Addition of a float and fixed object");
 
 	//subtraction
-	r = fix_i - Fixed(20);
-	std::cout << "## expected: 10, got: " << r << std::endl;
+	test(fix_i - Fixed(20), 10.0f, "Subtraction of two fixed objects");
+	test(fix_f - Fixed(20), 0.5f, "Subtraction of two fixed objects");
+	test(fix_f - f, 18.0f, "Subtraction of a float from a fixed object");
+	test(fix_i - i, 20.0f, "Subtraction of an int from a fixed object");
+	test(i - fix_i, -20.0f, "Subtraction of a fixed object from an int");
+	test(f - fix_i, -27.5f, "Subtraction of a fixed object from a float");
 
-	r = fix_f - Fixed(20);
-	std::cout << "## expected: 0.5, got: " << r << std::endl;
-
-	r = fix_f - f;
-	std::cout << "## expected: 18, got: " << r << std::endl;
-
-	r = fix_i - i;
-	std::cout << "## expected: 20, got: " << r << std::endl;
-
-	r = i - fix_i;
-	std::cout << "## expected: -20, got: " << r << std::endl;
-
-	r = f - fix_i;
-	std::cout << "## expected: -27.5, got: " << r << std::endl;
 
 	//multiplication
-	r = fix_i * Fixed(10);
-	std::cout << "## expected: 300, got: " << r << std::endl;
+	test(fix_i * Fixed(10), 300.0f, "Multiplication of two fixed objects");
+	test(fix_f * Fixed(20), 410.0f, "Multiplication of two fixed objects");
+	test(fix_f * 1.5f, 30.75f, "Multiplication of a float and a fixed object");
+	test(fix_i * 2, 60.0f, "Multiplication of an int and a fixed object");
+	test(0.5f * fix_i, 15.0f, "Multiplication of a float and a fixed object");
+	test(2 * fix_i, 60.0f, "Multiplication of an int and a fixed object");
 
-	r = fix_f * Fixed(20);
-	std::cout << "## expected: 410, got: " << r << std::endl;
-
-	r = fix_f * 1.5f;
-	std::cout << "## expected: 30.75, got: " << r << std::endl;
-
-	r = fix_i * 2;
-	std::cout << "## expected: 60, got: " << r << std::endl;
-
-	r = 0.5f * fix_i;
-	std::cout << "## expected: 15, got: " << r << std::endl;
-
-	r = 2 * fix_i;
-	std::cout << "## expected: 60, got: " << r << std::endl;
 
 	//division
-	r = fix_i / Fixed(10);
-	std::cout << "## expected: 3, got: " << r << std::endl;
+	test(fix_i / Fixed(10), 3.0f, "Division of two fixed objects");
+	test(fix_f / Fixed(0.5f), 41.0f, "Division of two fixed objects");
+	test(fix_i / 10, 3.0f, "Division of a fixed object by an int");
+	test(fix_i / 2.5f, 12.0f, "Division of a fixed object by a float");
+	test(60 / fix_i, 2.0f, "Division of an int by a fixed object");
+	test(92.25f / fix_f, 4.5f, "Division of a float by a fixed object");
 
-	r = fix_f / Fixed(2.5f);
-	std::cout << "## expected: 8.2, got: " << r << std::endl;
-
-	r = fix_i / 10;
-	std::cout << "## expected: 3, got: " << r << std::endl;
-
-	r = fix_f / 10.25f;
-	std::cout << "## expected: 2, got: " << r << std::endl;
-
-	r = 60 / fix_i;
-	std::cout << "## expected: 2, got: " << r << std::endl;
-
-	r = 92.25f / fix_f;
-	std::cout << "## expected: 4.5, got: " << r << std::endl;
-
-	r = 92.25f / 0;
-	std::cout << "## expected: 0, got: " << r << std::endl;
+	//r = 92.25f / 0;
+	//std::cout << "## expected: 0, got: " << r << std::endl;
 
 	//Comparison
-	bool test = fix_i > fix_f;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = fix_i > 0;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-
-	test = fix_i > 1.5f;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = 0 > fix_i;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	test = 1.5f > fix_i;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	
-	test = fix_i < 0;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	test = fix_i < 1.5f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = fix_i < fix_f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = 0 < fix_i;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = 12.5f < fix_f;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
+	test_comp(fix_i > fix_f, true, "Testing >: comparing two fixed objects");
+	test_comp(fix_i > 0, true, "Testing >: comparing fixed object with an int");
+	test_comp(fix_i > 1.5f, true, "Testing >: comparing fixed object with a float");
+	test_comp(1.5f > fix_i, false, "Testing >: comparing  float with a fixed object");
+	test_comp(0 > fix_i, false, "Testing >: comparing int with a fixed object");
+	test_comp(1.5f > fix_i, false, "Testing >: comparing float with a fixed object");
 
 
-	test = fix_i >= fix_f;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = fix_i >= 0;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-
-	test = fix_i >= 1.5f;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-
-	test = 0 >= fix_i;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	test = 1.5f >= fix_i;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-
-	test = fix_i <= 0;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	test = fix_i <= 1.5f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = fix_i <= fix_f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = 0 <= fix_i;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = 12.5f <= fix_f;
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	
-	test = fix_i == 0;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	test = fix_i == 1.5f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = fix_i == fix_f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = 0 == fix_i;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-	
-	test = 12.5f == fix_f;
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
+	test_comp(fix_i < 0, false, "Testing <: comparing fixed object with an int");
+	test_comp(fix_i < 1.5f, false, "Testing <: comparing fixed object with a float");
+	test_comp(fix_i < fix_f, false, "Testing <: comparing two fixed objects");
+	test_comp(0 < fix_i, true, "Testing <: comparing int with a fixed object");
+	test_comp(12.5f < fix_f, true, "Testing <: comparing float with a fixed object");
 
 	
-	test = (fix_i != 30);
-	std::cout << "## expected: 0 (false), got: " << test << std::endl;
-
-	test = (fix_i != 1.5f);
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = (fix_i != fix_f);
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = (0 != fix_i);
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
-	
-	test = (12.5f != fix_f);
-	std::cout << "## expected: 1 (true), got: " << test << std::endl;
+	test_comp(fix_i >= 0, true, "Testing >=: comparing fixed object with an int");
+	test_comp(fix_i >= 1.5f, true, "Testing >=: comparing fixed object with a float");
+	test_comp(fix_i >= fix_f, true, "Testing >=: comparing two fixed objects");
+	test_comp(0 >= fix_i, false, "Testing >=: comparing int with a fixed object");
+	test_comp(1.5f >= fix_i, false, "Testing >=: comparing float with a fixed object");
 
 
+	test_comp(fix_i <= 0, false, "Testing <=: comparing fixed object with an int");
+	test_comp(fix_i <= 1.5f, false, "Testing <=: comparing fixed object with a float");
+	test_comp(fix_i <= fix_f, false, "Testing <=: comparing two fixed objects");
+	test_comp(0 <= fix_i, true, "Testing <=: comparing int with a fixed object");
+	test_comp(12.5f <= fix_f, true, "Testing <=: comparing float with a fixed object");
+
+
+	test_comp(fix_i == 0, false, "Testing ==: comparing fixed object with an int");
+	test_comp(fix_i == 1.5f, false, "Testing ==: comparing fixed object with a float");
+	test_comp(fix_i == fix_f, false, "Testing ==: comparing two fixed objects");
+	test_comp(0 == fix_i, false, "Testing ==: comparing int with a fixed object");
+	test_comp(12.5f == fix_f, false, "Testing ==: comparing float with a fixed object");
+
 	
-	/*std::cout << b << std::endl;
-	Fixed const c( 3.3f + Fixed(30 ));
-	std::cout << c << std::endl;*/
-	/*std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;*/
+	test_comp(fix_i != 30, false, "Testing !=: comparing fixed object with an int");
+	test_comp(fix_i != 1.5f, true, "Testing !=: comparing fixed object with a float");
+	test_comp(fix_i != fix_f, true, "Testing !=: comparing two fixed objects");
+	test_comp(0 != fix_i, true, "Testing !=: comparing int with a fixed object");
+	test_comp(12.5f != fix_f, true, "Testing !=: comparing float with a fixed object");
+
+
+
 	return 0;
 }
