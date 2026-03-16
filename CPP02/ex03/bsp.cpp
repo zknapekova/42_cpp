90c8b489d71f70cfe9808eecaca276349aad6eba
Zuzana Knapekova <z.knapekova@gmail.com>
2026-03-16 16:07:41 +0100

    finish ex03, add tests

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -13,19 +13,29 @@
 
 #include "Point.hpp"
 
-int	is_vertex(Point const a, Point const b, Point const c, Point const point)
-{
-	if ((a.getX() == point.getX() && a.getY() == point.getY()) || \
-(b.getX() == point.getX() && b.getY() == point.getY()) || \
-(c.getX() == point.getX() && c.getY() == point.getY()))
-		return 1;
-	return 0;
 
+static Fixed	cross_product(Point const &p, Point const &q, Point const &r)
+{
+	return (q.getX() - p.getX())*(r.getY()-p.getY()) - \
+(q.getY() - p.getY())*(r.getX()-p.getX());
+}
+
+static bool	is_positive(Fixed const &f)
+{
+	return (f.toInt() > 0);
 }
 
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	if (is_vertex(a, b, c, point))
+		
+	Fixed cp_ab = cross_product(a, b, point);
+	Fixed cp_bc = cross_product(b, c, point);
+	Fixed cp_ca = cross_product(c, a, point);
+	
+	if (cp_ab.toInt() == 0 || cp_ca.toInt() == 0 || cp_bc.toInt() == 0)
 		return false;
+	if (is_positive(cp_ab) == is_positive(cp_ca) && is_positive(cp_ca) == is_positive(cp_bc))
+		return true;
+	return false;	
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -19,10 +19,26 @@ int main( void ) {
 	Point p = Point();
 	std::cout << p << std::endl;
 	
-	Point q = Point(1.5f, 0.5f);
-	std::cout << q << std::endl;
+	Point a = Point(0.0f, 0.0f);
+	Point b = Point(4.0f, 0.0f);
+	Point c = Point(0.0f, 4.0f);
 
+	std::cout << b << std::endl;
+
+	// point inside the triangle
+	bool res1 = bsp(a, b, c, Point(1.0f, 1.0f));
+	std::cout << "1.result is " << res1 << std::endl;
 	
+	//point outside the triangle
+	bool res2 = bsp(a, b, c, Point(5.0f, 5.0f));
+	std::cout << "2.result is " << res2 << std::endl;
 	
+	//point is one of the vertices
+	bool res3 = bsp(a, b, c, Point(0.0f, 0.0f));
+	std::cout << "3.result is " << res3 << std::endl;
+	
+	//point is on the edge
+	bool res4 = bsp(a, b, c, Point(3.0f, 0.0f));
+	std::cout << "4.result is " << res4 << std::endl;
 	return 0;
 }
